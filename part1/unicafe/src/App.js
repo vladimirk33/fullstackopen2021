7bbd29b6085672ae9812aeabf444e329e8735fde
Vladimir <mr.katenin@mail.ru>
2021-10-14 17:26:54 +0300

    unicafe steps3,4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  if (total > 0) {
+    return (
+      <>
+        <p>
+          good {good}<br></br>
+          neutral {neutral}<br></br>
+          bad {bad}<br></br>
+          all {good + neutral + bad}<br></br>
+          average {(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)}<br></br>
+          positive {(good) * 100 / (good + neutral + bad)} %
+        </p>
+      </>
+    )
+  }
+  return <p>No feedback given</p>
+}
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
-)
-
-const Text = ({ good, neutral, bad }) => (
-  <p>
-    good {good}<br></br>
-    neutral {neutral}<br></br>
-    bad {bad}<br></br>
-    all {good + neutral + bad}<br></br>
-    average {(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)}<br></br>
-    positive {(good) * 100 / (good + neutral + bad)} %
-  </p>
 )
 
 const App = () => {
@@ -30,7 +38,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Text good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
