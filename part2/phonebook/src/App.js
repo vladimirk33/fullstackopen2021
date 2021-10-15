a1e000125015b868dcf5f1a1d0755d57945c5cf2
Vladimir <mr.katenin@mail.ru>
2021-10-16 00:38:04 +0300

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearchingName, setNewSearchingName ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.map(person => person.name).includes(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else if (persons.map(person => person.number).includes(newNumber)) {
-      window.alert(`Number ${newNumber} is already added to phonebook`)
+      window.alert(`Phone number ${newNumber} is already added to phonebook`)
     } else {
       const personObject = {
         name: newName,
@@ -32,18 +36,32 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchingNameChange = (event) => {
+    setNewSearchingName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with
+      <input value={newSearchingName} onChange={handleSearchingNameChange}/>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
-        <p key={person.name}>{person.name} {person.number}</p>
-      )}
+      {newSearchingName === ""
+        ? persons.map(person =>
+            <p key={person.name}>{person.name} {person.number}</p>
+          )
+        : persons.filter(person =>
+            person.name.toLowerCase().includes(newSearchingName.toLowerCase())
+          ).map(person =>
+            <p key={person.name}>{person.name} {person.number}</p>
+          )
+      }
     </div>
   )
 }
