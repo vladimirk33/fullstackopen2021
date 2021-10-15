ae1ba03748c7275d30833ea6f734f317d68713c5
Vladimir <mr.katenin@mail.ru>
2021-10-15 20:30:37 +0300

    part2 init

--- other/src/App.js
+++ other/src/App.js
@@ -1,37 +1,18 @@
-import React, { useState } from "react";
+import React from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
+      <h1>Notes</h1>
+      {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
     </div>
   )
 }
 
-export default App;
+export default App
