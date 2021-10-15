f6d595b91493d1caa2893a2c9acd9d3f0332d9af
Vladimir <mr.katenin@mail.ru>
2021-10-15 21:30:25 +0300

    courseinfo step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -49,7 +49,7 @@ const App = () => {
   return (
     <div>
       {courses.map(course =>
-      <Course key={course.id} course={course} />
+        <Course key={course.id} course={course} />
       )}
     </div>
   )
