39a5ef91887cbc1975599d5faedaafe8d8857740
Vladimir <mr.katenin@mail.ru>
2021-10-15 22:29:49 +0300

    phonebook step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,38 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addNumber = (event) => {
+    event.preventDefault()
+    const numberObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(numberObject))
+    setNewName('')
+  }
+
+  const handleNumberChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNumber} >
         <div>
-          name: <input />
+          name: <input 
+                  value={newName}
+                  onChange={handleNumberChange}
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <p key={person.name}>{person.name}</p>
+      )}
     </div>
   )
 }
