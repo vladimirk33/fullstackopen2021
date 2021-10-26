48ac7b5b8e4c41504dbaeb920297f1f361b77ffe
Vladimir <mr.katenin@mail.ru>
2021-10-26 20:46:57 +0300

    phonebook step10

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -9,7 +9,17 @@ const PersonForm = ({ persons, setPersons }) => {
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.map(person => person.name).includes(newName)) {
-      window.alert(`${newName} is already added to phonebook`)
+      const currPerson = persons.filter(person => person.name === newName)[0]
+      if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)) {
+        const changedPersonObject = {
+          ...currPerson, number: newNumber
+        }
+        Data
+          .update(currPerson.id, changedPersonObject)
+          .then(response => {
+            setPersons(persons.map(person => person.name !== currPerson.name ? person : response.data))
+          })
+      }
     } else if (persons.map(person => person.number).includes(newNumber)) {
       window.alert(`Phone number ${newNumber} is already added to phonebook`)
     } else {
